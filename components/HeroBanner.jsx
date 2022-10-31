2360ecc395b91cc42e50a38c902be9b8a273ba1c
ady.to <adiyulianto61@gmail.com>
2022-10-31 22:17:29 +0700

    responsive herobanner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -5,17 +5,19 @@ import { urlFor } from "../lib/client";
 
 const HeroBanner = ({ heroBanner }) => {
   return (
-    <div className="py-24 px-10 bg-gray-300 rounded-2xl relative h-[500px] w-full">
+    <div className="py-10 px-4 xs:py-14 xs:px-5 lg:py-24 lg:px-10 bg-gray-300 rounded-2xl relative h-72 xs:h-96 lg:h-[500px] w-full">
       <div>
-        <p className="text-xl">{heroBanner.smallText}</p>
-        <h1 className="text-6xl mt-1 font-bold">{heroBanner.midText}</h1>
-        <h2 className="text-white text-[10em] -ml-5 leading-none font-bold">
+        <p className="text-sm xs:text-sm lg:text-xl">{heroBanner.smallText}</p>
+        <h1 className="text-3xl xs:text-4xl lg:text-6xl mt-1 font-bold">
+          {heroBanner.midText}
+        </h1>
+        <h2 className="text-white text-5xl xs:text-8xl lg:text-[160px] -ml-1 xs:-ml-2 lg:-ml-5 leading-none font-bold">
           {heroBanner.largeText1}
         </h2>
         <img
           src={urlFor(heroBanner.image)}
           alt="headphones"
-          className="absolute top-0 right-1/4 w-[450px] h-[450px]"
+          className="absolute hidden sm:inline sm:top-0 sm:right-16 sm:w-96 sm:h-96 md:right-36 lg:top-0 lg:right-[15%] lg:w-[450px] lg:h-[450px] xl:right-[25%]"
         />
         <div>
           <Link href={`/product/${heroBanner.product}`}>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      xs: "350px",
+      ...defaultTheme.screens,
+    },
     extend: {
       animation: {
         marquee: "marquee 15s linear infinite",
